9e265604ff2d26c938ee6362f939c4d84d991441
bikutech44 <92908935+bikutech44@users.noreply.github.com>
2021-10-21 12:22:53 +0545

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,6 +1,6 @@
 <?php
 $name = $_POST['name'];
-$visistor_email = $_POST['email'];
+$visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
  
@@ -18,7 +18,7 @@ $to = 'bikutech44@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
-$headers .= "Reply-To: $visistor_email \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
     
 
 mail($to,$email_subject,$email_body,$headers);
