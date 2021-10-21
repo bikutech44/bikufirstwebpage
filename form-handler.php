7d23c1f6377a96962ec4ce8e79f93aae36586eec
bikutech44 <92908935+bikutech44@users.noreply.github.com>
2021-10-21 11:55:24 +0545

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $POST[''];
+$visistor_email = $POST[''];
+$subject = $POST[''];
+$message = $POST[''];
+ 
+$email_from ='bikufirstwebpage.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n"
+                "User Email: $visistor_email.\n"
+                "Subject: $subject.\n"
+              "User Message: $message.\n";
+
+
+$to = 'bikutech44@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visistor_email \r\n";
+    
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+    
+    
+?>
