8ac785622c012c80aef51b34b3ea43c8f7c13722
bikutech44 <92908935+bikutech44@users.noreply.github.com>
2021-10-22 17:47:16 +0545

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
  
-$email_from ='info@bikutech44.github.io';
+$email_from ='bikutech88@gmail.com';
 
 $email_subject = 'New Form Submission';
 
